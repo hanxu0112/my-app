a0216fd5c2dff7715f6671edb5ad52f157c5f2b2
HanXu <hanxuts@cn.ibm.com>
2020-12-04 13:41:15 +0800

    Revert "add react demo"
    This reverts commit 7c47553179ad4b09313702d8a82e8d498bbd6f52.

--- src/normal/components/Star.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {FaStar} from "react-icons/fa";
-
-const Star = ({ selected = false, onClick = (f) => f }) => (
-    <FaStar id="star" color={selected ? "red" : "grey"} onClick={onClick} />
-);
-export default Star;
--- src/normal/components/inputHook.js
+++
@@ -1,9 +0,0 @@
-import {useState} from 'react'
-
-export const UserInput =(initalValue) =>{
-    const [value,setValue] = useState(initalValue)
-    return [
-        { value,onChange:(e)=>setValue(e.target.value) },
-        ()=>setValue(initalValue)
-    ]
-}
